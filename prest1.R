50054896fc41fea7622755ee555b2a626d6ed9bd
LuisFFBM <101729535+LuisFFBM@users.noreply.github.com>
2024-04-24 19:59:38 +0200

    primera tabla hecha hasta histograma

--- prest1.R
+++ prest1.R
@@ -41,50 +41,31 @@ pie(total_nacimientos, labels = names(total_nacimientos), main = "Contribución 
 
 data <- read_excel("/home/luis/Escritorio/PRESTvis/PREST-2324-DatosTrabajo.xlsx", sheet = 1, skip = 8) 
 
-# Supongamos que las columnas están organizadas como sigue: Edades, Galicia, Madrid, Murcia, Galicia, Madrid, Murcia
+# Supongamos que las columnas están organizadas como sigue: Edades, Galicia Mujeres, Madrid Mujeres, Murcia Mujeres, Galicia Hombres, Madrid Hombres, Murcia Hombres
 data_clean_mujeres <- data[, c(1, 7:9)]
 data_clean_hombres <- data[, c(1, 12:14)]
-colnames(data_clean_mujeres) <- c("Edad", "Galicia", "Madrid", "Murcia")
-colnames(data_clean_hombres) <- c("Edad","Galicia", "Madrid", "Murcia")
+colnames(data_clean_mujeres) <- c("Edad", "Galicia Mujeres", "Madrid Mujeres", "Murcia Mujeres")
+colnames(data_clean_hombres) <- c("Edad","Galicia Hombres", "Madrid Hombres", "Murcia Hombres")
 
 # Convertir las columnas a tipo numérico
-data_clean_mujeres <- sapply(data_clean_mujeres, function(x) as.numeric(as.character(x)))
-data_clean_hombres <- sapply(data_clean_hombres, function(x) as.numeric(as.character(x)))
+data_clean_mujeres[,-1] <- sapply(data_clean_mujeres[,-1], function(x) as.numeric(as.character(x)))
+data_clean_hombres[,-1] <- sapply(data_clean_hombres[,-1], function(x) as.numeric(as.character(x)))
 
 # Diagramas de cajas para hombres y mujeres por comunidad
-boxplot(data_clean_mujeres[,c("Galicia", "Madrid", "Murcia")], 
+boxplot(data_clean_mujeres[,c("Galicia Mujeres", "Madrid Mujeres", "Murcia Mujeres")], 
         main = "Nacimientos de Mujeres por Comunidad",
         xlab = "Comunidad", ylab = "Nacimientos", col = c("pink", "red", "orange"), names = c("Galicia", "Madrid", "Murcia"))
 
-boxplot(data_clean_hombres[,c("Galicia", "Madrid", "Murcia")], 
+boxplot(data_clean_hombres[,c("Galicia Hombres", "Madrid Hombres", "Murcia Hombres")], 
         main = "Nacimientos de Hombres por Comunidad",
         xlab = "Comunidad", ylab = "Nacimientos", col = c("lightblue", "blue", "cyan"), names = c("Galicia", "Madrid", "Murcia"))
 
 # Calculando el total de nacimientos por sexo y comunidad
-total_mujeres <- colSums(data_clean_mujeres[, c("Galicia", "Madrid", "Murcia")], na.rm = TRUE)
-total_hombres <- colSums(data_clean_hombres[, c("Galicia", "Madrid", "Murcia")], na.rm = TRUE)
+total_mujeres <- colSums(data_clean_mujeres[, c("Galicia Mujeres", "Madrid Mujeres", "Murcia Mujeres")], na.rm = TRUE)
+total_hombres <- colSums(data_clean_hombres[, c("Galicia Hombres", "Madrid Hombres", "Murcia Hombres")], na.rm = TRUE)
 
-#analisis maternidad en distintas comunidades
+# Diagramas de sectores para mostrar la contribución por sexo y comunidad
+par(mfrow = c(1, 2))
+pie(total_mujeres, labels = names(total_mujeres), main = "Contribución de Mujeres al Total de Nacimientos", col = c("pink", "red", "orange"))
+pie(total_hombres, labels = names(total_hombres), main = "Contribución de Hombres al Total de Nacimientos", col = c("lightblue", "blue", "cyan"))
 
-# Asumiendo que data_clean es el DataFrame con las columnas adecuadas
-# Ejemplo de nombres de columnas: "Edad", "Galicia", "Madrid", "Murcia"
-
-# Transformar frecuencias en listas de edades
-
-edad_galicia <- rep(data_clean_mujeres[, "Edad"], times = data_clean_mujeres[, "Galicia"])
-edad_madrid <- rep(data_clean_mujeres[, "Edad"], times = data_clean_mujeres[, "Madrid"])
-edad_murcia <- rep(data_clean_mujeres[, "Edad"], times = data_clean_mujeres[, "Murcia"])
-
-
-# Calcular estadísticas descriptivas
-summary_galicia <- summary(edad_galicia)
-summary_madrid <- summary(edad_madrid)
-summary_murcia <- summary(edad_murcia)
-
-# Visualización con histogramas
-hist(edad_galicia, breaks = 30, main = "Distribución de Edad de Maternidad en Galicia", xlab = "Edad")
-hist(edad_madrid, breaks = 30, main = "Distribución de Edad de Maternidad en Madrid", xlab = "Edad")
-hist(edad_murcia, breaks = 30, main = "Distribución de Edad de Maternidad en Murcia", xlab = "Edad")
-
-# Diagramas de cajas para comparar
-boxplot(edad_galicia, edad_madrid, edad_murcia, names = c("Galicia", "Madrid", "Murcia"), main = "Comparación de Edad de Maternidad por Comunidad")
